374d195aa2b3d5f163f9b7e5276e8e6b5cfcd952
Hurbology <owen@elderado.co.uk>
2026-07-14 20:21:57 +0100

    A: wrote the _bootstrap, load, and save functions using the modules 'pathlib' and 'json'

--- backlog/storage.py
+++ backlog/storage.py
@@ -1,7 +1,24 @@
 import json
 from pathlib import Path
 
-DEFAULT_PATH = Path.home() / ".backlog.json"
+BASE_DIR = Path.home() / ".backlog-tracker-cli"
+DEFAULT_PATH = BASE_DIR / ".backlog.json"
+
+_is_initialized = False
+
+def _bootstrap():
+    global _is_initialized
+
+    if _is_initialized:
+        return
+    
+    # Initialize the storage directory if it doesn't exist
+    if not BASE_DIR.exists():
+        BASE_DIR.mkdir(parents=True)
+        print(f"Created storage directory '.backlog-tracker-cli' at {BASE_DIR}")
+    
+    _is_initialized = True
+    
 
 def load(path: Path = DEFAULT_PATH):
     try:
@@ -10,4 +27,10 @@ def load(path: Path = DEFAULT_PATH):
         return [] # on first run backlog.json file doesn't exist, instead of crashing, return an empty list
 
 def save(games, path: Path = DEFAULT_PATH):
+    _bootstrap() # Initialize the storage directory if it doesn't exist then becomes a no-op on subsequent calls
+
+    is_new_file = not path.exists() 
     path.write_text(json.dumps(games, indent=2)) # turns the python list of dicts into a readable json file and saves it to the default path
+
+    if is_new_file:
+        print(f"Created backlog.json file at {path}")
